e1d7cbedc70986d169ac8164d336508cd6f3223c
hanh2fpts <hanh2@fpts.com.vn>
2023-05-23 14:51:38 +0700

    add firebase crash

--- lib/overview_page.dart
+++ lib/overview_page.dart
@@ -1,5 +1,10 @@
+import 'package:dio/dio.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:gallery_saver/gallery_saver.dart';
+import 'package:path_provider/path_provider.dart';
 
 class OverviewPage extends StatefulWidget {
   const OverviewPage({super.key});
@@ -30,7 +35,9 @@ class _OverviewPageState extends State<OverviewPage> {
                   final file = files[index];
                   return ListTile(
                     title: Text(file.name),
-                    trailing: IconButton(onPressed: () {}, icon: const Icon(Icons.download)),
+                    trailing: IconButton(
+                        onPressed: () => compute(downloadFile, file),
+                        icon: const Icon(Icons.download)),
                   );
                 });
           } else if (snapshot.hasError) {
@@ -42,5 +49,26 @@ class _OverviewPageState extends State<OverviewPage> {
         },
       ),
     );
+  }
+
+  static Future downloadFile(Reference ref) async {
+    try {
+      final url = await ref.getDownloadURL();
+      final tempDir = await getTemporaryDirectory();
+      final path = '${tempDir.path}/${ref.name}';
+      await Dio().download(url, path);
+      if (url.contains('.mp4')) {
+        await GallerySaver.saveVideo(path, toDcim: true);
+      } else if (url.contains('.jpg')) {
+        await GallerySaver.saveImage(path, toDcim: true);
+      }
+    } catch (e) {
+      FirebaseAnalytics.instance.logEvent(name: 'test', parameters: {'content': e});
+    }
+    //showMessage(ref);
+  }
+
+  void showMessage(Reference ref) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Download ${ref.name}')));
   }
 }
